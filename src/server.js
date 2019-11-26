f7b30c2d93b492b1053e983329000f1ee526edc5
nkwei15 <45564333+nkwei15@users.noreply.github.com>
2019-11-26 20:35:07 +0800

    Update server.js

--- src/server.js
+++ src/server.js
@@ -90,10 +90,10 @@ app.patch('/update/:postID', async (req,res)=>{
 });
 
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static('NewsWeatherApp/build'));
+    app.use(express.static(path.join(__dirname, 'build')));
 
     app.get('*', (req,res)=>{
-    res.sendFile(path.resolve(__dirname,"NewsWeatherApp","build","index.html"))
+    res.sendFile(path.join(__dirname,"build","index.html"))
     })
 
 }
