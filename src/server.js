27d56620e51e27b070ab6ed9e2765f0a40f917c9
nkwei15 <45564333+nkwei15@users.noreply.github.com>
2019-11-26 20:43:41 +0800

    Update server.js

--- src/server.js
+++ src/server.js
@@ -90,7 +90,7 @@ app.patch('/update/:postID', async (req,res)=>{
 });
 
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, 'build')));
+    app.use(express.static('/build/'));
 
     app.get('*', (req,res)=>{
     res.sendFile(path.join(__dirname,"build","index.html"))
